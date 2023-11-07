5ad30096904816272b4bfe4bdbc8e172a5edafa1
Yoann Bonge <137085636+yoannBonge@users.noreply.github.com>
2023-11-07 10:11:35 +0100

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Header = () => {
+  return (
+    <div>
+      <img src='/images/logo-kasa.png' alt='logo Kasa' />
+      <Navigation />
+    </div>
+  );
+};
+
+export default Header;
